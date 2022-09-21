803a74b4be5e3ff1212870c4a6042072694d42b1
Christian Roese <croese@8thlight.com>
2022-09-21 16:17:04 -0400

    Adds for loop to parser

--- Nlox.Core/Parser.cs
+++ Nlox.Core/Parser.cs
@@ -46,6 +46,10 @@ public class Parser {
     }
 
     private Stmt Statement() {
+        if (Match(TokenType.FOR)) {
+            return ForStatement();
+        }
+
         if (Match(TokenType.IF)) {
             return IfStatement();
         }
@@ -63,6 +67,53 @@ public class Parser {
         }
 
         return ExpressionStatement();
+    }
+
+    private Stmt ForStatement() {
+        Consume(TokenType.LEFT_PAREN, "expect '(' after 'for'");
+
+        Stmt initializer;
+        if (Match(TokenType.SEMICOLON)) {
+            initializer = null;
+        }
+        else if (Match(TokenType.VAR)) {
+            initializer = VarDeclaration();
+        }
+        else {
+            initializer = ExpressionStatement();
+        }
+
+        Expr condition = null;
+        if (!Check(TokenType.SEMICOLON)) {
+            condition = Expression();
+        }
+
+        Consume(TokenType.SEMICOLON, "expect ';' after loop condition");
+
+        Expr increment = null;
+        if (!Check(TokenType.RIGHT_PAREN)) {
+            increment = Expression();
+        }
+
+        Consume(TokenType.RIGHT_PAREN, "expect ')' after for clauses");
+
+        var body = Statement();
+
+        if (increment != null) {
+            body = new Stmt.Block(new List<Stmt> { body, new Stmt.Expression(increment) });
+        }
+
+        if (condition == null) {
+            condition = new Expr.Literal(true);
+        }
+
+        body = new Stmt.While(condition, body);
+
+        if (initializer != null) {
+            body = new Stmt.Block(new List<Stmt> { initializer, body });
+        }
+
+        return body;
     }
 
     private Stmt WhileStatement() {
